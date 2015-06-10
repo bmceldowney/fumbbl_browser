ecd49b6b8f113aeb356e9d478ffba1ce8278849a
Ben McEldowney <bmceldowney@weareratio.com>
2015-06-09 17:08:38 -0700

    adding roster info

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -51,19 +51,48 @@ angular.module('application').controller('mainCtrl', function ($scope, $state, f
 
 'use strict';
 
-angular.module('application').controller('playerDetailCtrl', function ($stateParams, $scope, fumbblData) {
+angular.module('application').controller('playerDetailCtrl', function ($q, $stateParams, $scope, fumbblData, utils) {
   var playerId = $stateParams.playerId;
   var teamId = $stateParams.teamId;
 
-  fumbblData.getTeamDataById(teamId).then(function (teamData) {
+  fumbblData.getTeamDataById(teamId)
+    .then(setTeamData)
+    .then(setRosterData)
+
+  function setRosterData(roster) {
+    var position = roster.position.filter(function (position) {
+      return position['@id'] === $scope.player.positionId;
+    });
+
+    if (position.length) {
+      $scope.position = position[0];
+    }
+
+    var attributes = {
+      movement: $scope.position.movement,
+      strength: $scope.position.strength,
+      agility: $scope.position.agility,
+      armorValue: $scope.position.armour,
+    };
+
+    $scope.attributes = attributes;
+
+    console.dir($scope.position);
+  }
+
+  function setTeamData (teamData) {
     var players = teamData.player.filter(function (playerData) {
       return playerData['@id'] === playerId;
     });
 
     $scope.player = players[0];
     $scope.stats = $scope.player.playerStatistics;
+
     console.dir($scope.player);
-  });
+
+    return fumbblData.getRosterById(teamData.rosterId);
+  }
+
 });
 'use strict';
 
